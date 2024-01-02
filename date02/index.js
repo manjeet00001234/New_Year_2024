3e0c804eddc2d51c4fd4784af2c4a0432da4c298
manjeet <manjeet>
2024-01-02 15:59:16 +0530

    update code

--- date02/index.js
+++ date02/index.js
@@ -8,3 +8,21 @@ else if(a>=45){
 else if(a<45){
     console.log("you are therd division");
 }
+
+
+
+
+
+
+
+var row = 5;
+var col =5;
+console.log('<table>')
+    for(i=1;i<=row;i++){
+        console.log('<tr>')
+            for(j=1;j<=col;j++){
+                console.log('<td>'+i,j +'</td>')
+            }
+            console.log('</tr>')
+    }
+    console.log('</table>')
