3443ae95b2d03f82c32aa8f1f1b61679e61e23de
manjeet00001234 <153909489+manjeet00001234@users.noreply.github.com>
2024-01-02 16:00:24 +0530

    Update index.js

--- date02/index.js
+++ date02/index.js
@@ -15,14 +15,4 @@ else if(a<45){
 
 
 
-var row = 5;
-var col =5;
-console.log('<table>')
-    for(i=1;i<=row;i++){
-        console.log('<tr>')
-            for(j=1;j<=col;j++){
-                console.log('<td>'+i,j +'</td>')
-            }
-            console.log('</tr>')
-    }
-    console.log('</table>')
+
