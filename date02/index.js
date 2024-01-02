0b992311c6ae13d7aff520ac5c1244d6dcc8a46b
manjeet <manjeet>
2024-01-02 11:04:38 +0530

    Update code

---
+++ date02/index.js
@@ -0,0 +1,10 @@
+let a = 40;
+if(a>=60){
+    console.log("you are first division");
+}
+else if(a>=45){
+    console.log("you are second division");
+}
+else if(a<45){
+    console.log("you are therd division");
+}
