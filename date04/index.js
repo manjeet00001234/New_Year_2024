36187a77881fe890cc9124b1b91646f6e98a2775
manjeet <manjeet>
2024-01-04 17:26:53 +0530

    update code 04

---
+++ date04/index.js
@@ -0,0 +1,100 @@
+// let gender = "male";
+// let age =  19;
+
+// if(gender =="male"){
+//     if(age >= 21){
+//         console.log("male marred");
+//     }else{
+//         console.log("male note marred");
+//     }
+// }else{
+//     if(gender == "female"){
+//         if(age >= 18){
+//             console.log("female marred");
+//         }else{
+//             console.log("female not marred");
+//         }
+//     }
+// }
+
+
+// let gender = "female";
+// let age = 18;
+
+// if((gender == "male" && age >=21) || (gender == "female" && age >=18)){
+//     console.log("valid for marrege");
+// }else{
+//     console.log("not valid for marrege");
+// }
+    
+
+
+
+// let day = "tuesday";
+
+// switch (day){
+//     case "monday":
+//         console.log("monday");
+//         break;
+
+//         case "tuesday":
+//         console.log("tuesday");
+//         break;
+
+//         case "weadnesday":
+//         console.log("weadnesday");
+//         break;
+
+//         default:
+//             console.log("not a day");
+// }
+
+
+// let day = 2;
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//         case 2:
+//             console.log("Tuesday");
+//             break;
+//             case 3:
+//                 console.log("Wednesday");
+//                 break;
+//                 case 4:
+//                     console.log("Thursday");
+//                     break;
+//                     case 5:
+//                         console.log("Friday");
+//                         break;
+//                         case 6:
+//                             console.log("Saturday");
+//                             break;
+//                             case 7:
+//                                 console.log("Sunday");
+//                                 break;
+//                                 default:
+//                                     console.log("Your day not define");
+// }
+
+
+// let Day_Name = "tuesday";
+
+// switch(Day_Name){
+//     case "monday":
+//     case "tuesday":
+//     case "wednesday":
+//     case "Thursday":
+//     case "friday":
+//     case "starday":
+//     case "sunday":
+//         console.log(`This is a day ${Day_Name}`);
+//         break;
+//     default:
+//         console.log("this is a wrong day");
+// }
+
+
+
+let city = "jaipur";
+city === "jaipur"?console.log("city is jaipur"):console.log("city in not");
