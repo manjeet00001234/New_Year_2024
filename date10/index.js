c20ce45d3ae595b7b4b85bfa3a8ef3ab6652a361
manjeet <manjeet>
2024-01-10 17:57:45 +0530

    update code

---
+++ date10/index.js
@@ -0,0 +1,98 @@
+// function five_even (num) {
+//     let count = 0;
+//     for (i = num; count < 5; i++){
+//         if (i % 2 == 0) {
+//             count++;
+//             console.log(i);
+//         }
+//         // if (count == 5) {
+//         //     break;
+//         // }
+//     }
+// }
+// five_even(20);
+
+
+// let a = "rajasthan police jaipur";
+// for (i = 0; i < a.length; i++){
+//     console.log(a);
+// }
+
+// let a = "";
+// for (i = 0; i <= 2; i++){
+//     for (i = 0; i <= 2; i++){
+//         a=a+"*"
+//         console.log(a);
+//     }
+//     // console.log("hello");
+// }
+
+
+
+// for (let i = 1; i <= 5; i++){
+//     let fild = "";
+//     for (let j = 0; j <= 5; j++){
+//         fild = fild + "*";
+//     }
+//     console.log(fild,`fild ${i}`);
+// }
+
+
+// for (let row = 5   ; row >= 1; row--){
+//     let bag = "";
+//     for (col = 1; col <= row; col++){
+//         bag = bag + "*";
+//     }
+//     console.log(bag);
+// }
+
+
+// for (i = 0; i <= 5; i++){
+//     for (j = 0; j < i; j++){
+//         // console.log(i);
+//         console.log(i,j);
+//     }
+// }
+
+
+
+
+// for (i = 5; i > 0;i--){
+//     console.log(i);
+// }
+
+
+// let num = 5;
+// for (i = 1; i <= num; i++){
+//     let bag = "";
+//     for (j = 1; j <= num; j++){
+//         if (j == 1 || j == 5) {
+//             bag = bag + "*";
+//         } else if(i==1 || i==5){
+//             bag = bag + "*";
+//         } else {
+//             bag = bag + "-";
+//         }
+//     }
+//     console.log(bag);
+// }
+
+
+
+// let num = 5;
+// for (i = 1; i <= num; i++){
+//     let bag = "";
+//     for (j = 1; j <= num; j++){
+//         if (i == 1 || i == 3 || i == 5) {
+//             bag = bag + "*";
+//         } else if (i==2 && j==1) {
+//             bag=bag+"*"
+//         } else if (i==4 && j==5) {
+//             bag=bag+"*"
+//         }
+//         else {
+//             bag=bag+" "
+//         }
+//     }
+//     console.log(bag);
+// }
